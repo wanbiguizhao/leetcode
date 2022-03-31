58eb0082f9ba105155fafa8882f2da193b4854e0
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-01 00:13:13 +0800

    ac 2191 sorted special data

---
+++ 2191. Sort the Jumbled Numbers.py
@@ -0,0 +1,20 @@
+from typing import List 
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        def convert(num):
+            ans=mapping[num%10]
+            power=0
+            if num==0:
+                return mapping[num]
+            while num>0:
+                ans=mapping[num%10]*10**power+ans
+                num=num//10
+                power+=1
+            return ans 
+        print(list(map(convert,nums)))
+        return sorted(nums ,key=lambda x: convert(x))
+
+if __name__=="__main__":
+    solution=Solution()
+    solution.sortJumbled([9,8,7,6,5,4,3,2,1,0],[0,9,8,7,6,5,4,3,2,1])  
+            
