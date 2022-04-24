3623d80e11c347141f0990686820f7129e9d56b4
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-25 01:27:00 +0800

    两种思路

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -5,18 +5,38 @@ class Solution:
         left_index=0
         right_index=len(nums)-1
         while left_index<=right_index:
+            # 搜索右侧
             mid_index=(left_index+right_index)//2
             if nums[mid_index]==target:
                 return mid_index
             # 判断某个区间是有序的
             if nums[mid_index]<nums[right_index]:
-                if target> nums[mid_index] and target<nums[right_index] :
+                if target> nums[mid_index] and target<=nums[right_index] :
                     left_index=mid_index+1
                 else:
                     right_index=mid_index-1
             else:
-                if target>nums[left_index] and target<nums[mid_index]:
-                    left_index=mid_index-1
+                if target>=nums[left_index] and target<nums[mid_index]:
+                    right_index=mid_index-1
+                else:
+                    left_index=mid_index+1
+        return -1
+    def search(self, nums: List[int], target: int) -> int:
+        left_index=0
+        right_index=len(nums)-1
+        while left_index<=right_index:
+            mid_index=(left_index+right_index)//2
+            if nums[mid_index]==target:
+                return mid_index
+            # 判断某个区间是有序的
+            if nums[left_index]<=nums[mid_index]:# 是不是要等于? 要因为下取整，存在left_index==mid_index的可能性
+                if target> nums[mid_index] and target<=nums[right_index] :
+                    left_index=mid_index+1
+                else:
+                    right_index=mid_index-1
+            else:
+                if target>=nums[left_index] and target<nums[mid_index]:
+                    right_index=mid_index-1
                 else:
                     left_index=mid_index+1
         return -1
