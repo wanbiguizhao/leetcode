d9465864a6e4c3b60c50177030bfb710a87c917f
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-25 17:11:08 +0800

    ac 34

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -1,5 +1,6 @@
 from turtle import left
 from typing import List 
+#https://www.cnblogs.com/grandyang/p/4325648.html
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left_index=0
@@ -9,7 +10,7 @@ class Solution:
             mid_index=(left_index+right_index)//2
             if nums[mid_index]==target:
                 return mid_index
-            # 判断某个区间是有序的
+            # 判断某个区间是有高数意义上的连续的
             if nums[mid_index]<nums[right_index]:
                 if target> nums[mid_index] and target<=nums[right_index] :
                     left_index=mid_index+1
@@ -30,15 +31,15 @@ class Solution:
                 return mid_index
             # 判断某个区间是有序的
             if nums[left_index]<=nums[mid_index]:# 是不是要等于? 要因为下取整，存在left_index==mid_index的可能性
-                if target> nums[mid_index] and target<=nums[right_index] :
-                    left_index=mid_index+1
-                else:
+                if target>= nums[left_index] and target<nums[mid_index] :
                     right_index=mid_index-1
+                else:
+                   left_index=mid_index+1
             else:
-                if target>=nums[left_index] and target<nums[mid_index]:
-                    right_index=mid_index-1
-                else:
+                if target>nums[mid_index] and target<=nums[right_index]:
                     left_index=mid_index+1
+                else:
+                   right_index=mid_index-1
         return -1
 
 
