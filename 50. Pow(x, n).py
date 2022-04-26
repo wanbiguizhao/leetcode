63831869f73769769490d44a0a122695d84e8765
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-26 16:10:23 +0800

    ac 50 大步长，与小步长

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,39 @@
+from socket import errorTab
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1 
+        if x==0:
+            return 0
+        if x==1:
+            return x 
+        if n<0:
+            x=1/x
+            n=-n
+        ans=x 
+        multiply_value=x # 记录每次递增的变量值
+        multiply_n=1 # 记录每次递增的变量n
+        current_n=1 # 
+        while current_n<n:
+            if current_n+multiply_n<n: # 迈大步
+                ans=ans*multiply_value
+                current_n=current_n+multiply_n
+                multiply_n=multiply_n*2
+                multiply_value=multiply_value*multiply_value
+            else: # 回退到最小的不长
+                multiply_n=1
+                multiply_value=x 
+                ans=ans*multiply_value
+                current_n=current_n+multiply_n  
+        return ans
+if __name__ == "__main__":
+    instance=Solution()
+    print(instance.myPow(0,1))
+    print(instance.myPow(1,1))
+    print(instance.myPow(2,1))
+    print(instance.myPow(2,10))
+    print(instance.myPow(2,-2))
+    print(instance.myPow(4,-1))
+    print(instance.myPow(-1/2,-9))
