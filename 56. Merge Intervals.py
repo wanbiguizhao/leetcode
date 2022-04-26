fa1277b15d08e35d9aaa6da862865eaf039bc704
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-26 23:28:04 +0800

    ac 56 排序，使用部分结构里面的部分结果

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -1,6 +1,6 @@
 from typing import List 
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    def merge_bad(self, intervals: List[List[int]]) -> List[List[int]]:
         new_intervals=sorted(intervals)
         ans=[]
         index_i=index_j=0
@@ -25,8 +25,25 @@ class Solution:
                     ]
         )
         return ans 
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        new_intervals=sorted(intervals)
+        ans=[]
+        index_i=index_j=0
+        const_len=len(intervals)
+        max_right=new_intervals[index_j][1]
+        while index_j<const_len:
+            if not ans or ans[-1][1] < new_intervals[index_j][0]: 
+                ans.append(
+                    [
+                        new_intervals[index_j][0],
+                        new_intervals[index_j][1]
+                    ]
+                )
+            ans[-1][1]=max(ans[-1][1],new_intervals[index_j][1] )
+            index_j+=1
+        return ans
 if __name__ == "__main__":
     instance=Solution()
     # print(instance.merge(intervals = [[1,3],[2,6],[8,10],[15,18]]))
     # print(instance.merge(intervals = [[1,18]]))
-    print(instance.merge(intervals =[[1,4],[0,2],[3,5]]))
+    print(instance.merge(intervals = [[2,4],[1,5],[1,6],[2,14]]))
