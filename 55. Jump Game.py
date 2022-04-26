b88ce1b0e69b06df7d5f238c34b804f3537c1199
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-26 19:39:02 +0800

    ac 55 使用O(1)的空间复杂度

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -1,6 +1,7 @@
+
 from typing import List 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+    def canJump_1(self, nums: List[int]) -> bool:
         const_len=len(nums)
         nums[-1]=True
         index=const_len-2 
@@ -20,6 +21,23 @@ class Solution:
                 max_jump-=1
             index-=1
         return nums[0]
+    def canJump(self, nums: List[int]) -> bool:
+        const_len=len(nums)
+        jump_reached=[False]*const_len
+        jump_reached[-1]=True
+        for index in reversed(range(0,const_len-1)):
+            max_jump=nums[index]+index
+            if max_jump>=const_len-1:
+                jump_reached[index]=True
+                continue 
+            while max_jump>index:
+                if jump_reached[max_jump]:
+                    jump_reached[index]=True
+                    break
+                max_jump-=1
+        return jump_reached[0]
+
+
 if __name__ == "__main__":
     instance=Solution()
     print(instance.canJump([0]))
