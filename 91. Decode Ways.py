901fce79846ad872eae69b3358760c3a98ca0af0
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-27 23:58:47 +0800

    ac 91 但是时间比较长 排名30%

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -1,14 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         def _numDecodings(s):
+            if s in memory:
+                return memory[s]
             if not s:
-                return 1 
-            s_1=0
+                memory[s]=1
+                return memory[s] 
             if len(s)==1:
                 if s[0] in mapping:
-                    return 1 
+                    memory[s]=1
+                    return memory[s] 
                 else:
-                    return 0 
+                    memory[s]=0
+                    return memory[s]
             #len(s)>=2 
             s_1=0
             s_2=0
@@ -16,9 +20,11 @@ class Solution:
                 s_1=_numDecodings(s[1:])
             if s[0]+s[1] in mapping:
                 s_2=_numDecodings(s[2:])
-            return s_1+s_2
-        if s[0]==0:
+            memory[s]=s_1+s_2
+            return memory[s]
+        if s[0]=='0':
             return 0
+        memory={}
         mapping={} # mapping['A']='1' mapping['1']='A'
         for x in range(26):
             mapping[chr(x+ord('A'))]=str(x+1)
@@ -31,6 +37,7 @@ if __name__ == "__main__":
     instance.numDecodings("1"),
     instance.numDecodings("12"),
     instance.numDecodings("1122"),
-    instance.numDecodings("626")
+    instance.numDecodings("626"),
+    instance.numDecodings("111111111111111111111111111111111111111111111")
     )
         
