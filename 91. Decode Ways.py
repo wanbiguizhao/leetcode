6332d4c48c6d400c3bb5200ca083d6e8b3960cc3
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-28 00:37:29 +0800

    ac 91 把缓存和字母映射表放在一起，似乎快乐一点

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numDecodings(self, s: str) -> int:
+    def numDecodings_1(self, s: str) -> int:
         def _numDecodings(s):
             if s in memory:
                 return memory[s]
@@ -30,6 +30,35 @@ class Solution:
             mapping[chr(x+ord('A'))]=str(x+1)
             mapping[str(x+1)]=chr(x+ord('A'))
         return _numDecodings(s)
+    def numDecodings(self, s: str) -> int:
+        def _numDecodings(s):
+            if s in memory:
+                return memory[s]
+            if s[0]=="0":
+                memory[s]=0
+                return memory[s]
+            if len(s)==1:
+                memory[s]=0
+                return memory[s]
+            if len(s)==2:
+                if int(s)%10==0:
+                    memory[s]=0
+                else:
+                    memory[s]=1 # 这点十分重要，所有合法的len(s)==2 时，s 长度为2的都已经找到了。 00 01 -> 99 长度为2的只有11-19 21-26 其他的长度都为1 
+                return memory[s]
+            if 1<=int(s[:2]) and int(s[:2])<=26:
+                memory[s] = _numDecodings(s[1:]) + _numDecodings(s[2:])
+            else:
+                memory[s] = _numDecodings(s[1:])
+            return memory[s]
+        memory={}
+        for x in range(26):
+            memory[str(x+1)]=len(str(x+1))
+        memory['10']=1 # 已经把所有的合法数字组合找到。
+        memory['20']=1 # 
+        memory['']=0
+        memory['0']=0
+        return _numDecodings(s)
         
 if __name__ == "__main__":
     instance=Solution()
@@ -38,6 +67,9 @@ if __name__ == "__main__":
     instance.numDecodings("12"),
     instance.numDecodings("1122"),
     instance.numDecodings("626"),
-    instance.numDecodings("111111111111111111111111111111111111111111111")
+    instance.numDecodings("111111111111111111111111111111111111111111111"),
+    instance.numDecodings_1("111111111111111111111111111111111111111111111"),
+    instance.numDecodings_1("230"),
+    instance.numDecodings("230")
     )
         
