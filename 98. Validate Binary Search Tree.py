dbe94bde17086967e57ef505115c63b1d66f3fbd
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-29 18:37:08 +0800

    ac 98

---
+++ 98. Validate Binary Search Tree.py
@@ -0,0 +1,56 @@
+#Definition for a binary tree node.
+from typing import Optional 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    # 使用临时节点的方法
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True 
+        self.ans=True
+        self.xnode=None
+        self.mid_order(root)
+        return self.ans
+    def mid_order(self,root):
+        if not root or not self.ans:
+            return  
+        self.mid_order(root.left)
+        if not self.ans:
+            return 
+        # 根节点
+        if not self.xnode:
+            self.xnode=root
+        else:
+            if self.xnode.val>=root.val:
+                self.ans=False
+            else:
+                self.xnode=root
+        self.mid_order(root.right)
+class Solution1:
+    # 使用堆栈的方法
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True 
+        self.ans=True
+        node_stack=[]
+        self.mid_order(root,node_stack)
+        return self.ans
+    def mid_order(self,root,node_stack):
+        if not root or not self.ans:
+            return  
+        self.mid_order(root.left,node_stack)
+        if not self.ans:
+            return
+        # 根节点
+        if not node_stack:
+            node_stack.append(root.val)
+        else:
+            if node_stack[-1]>=root.val:
+                self.ans=False
+            else:
+                node_stack.pop(-1)
+                node_stack.append(root.val)
+        self.mid_order(root.right,node_stack)
