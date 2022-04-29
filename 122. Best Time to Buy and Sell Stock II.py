e41e2462e1158735dae00d659bd11107c8331572
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-30 01:06:30 +0800

    ac 122 简化算法，使用变量

--- 122. Best Time to Buy and Sell Stock II.py
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -1,6 +1,6 @@
 from typing import Optional ,List
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit_1(self, prices: List[int]) -> int:
         # 存在数学上的单调性
         # 1,3,5,7,6,5,9
         xstack=[]
@@ -12,7 +12,17 @@ class Solution:
                 ans+=price-xstack[-1]
             xstack=[price]
         return ans 
+    def maxProfit(self, prices: List[int]) -> int:
+        # 存在数学上的单调性
+        # 1,3,5,7,6,5,9
+        bottom_price=prices[0]
 
+        ans=0
+        for price in prices:
+            if bottom_price<price:
+                ans+=price-bottom_price
+            bottom_price=price
+        return ans 
 
 if __name__ == "__main__":
     instance=Solution()
