37f0a639c7776893b695fb144f401ac4e1d18a82
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-04-30 01:37:32 +0800

    ac 128 runtime 53.96% 学会新的初始化字典的方法

---
+++ 128. Longest Consecutive Sequence.py
@@ -0,0 +1,29 @@
+from typing import  List
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        ans=1
+        dict_suffix_len={num:1 for num in nums}
+        for num in nums:
+            if dict_suffix_len[num]==1: 
+                next_num=num+1 
+                while next_num in dict_suffix_len and dict_suffix_len[next_num]==1:
+                     next_num+=1
+                base=0
+                # 一种情况是next_num not in dict_suffix_len 
+                # 另一种情况是next_num  in dict_suffix_len 
+                if next_num in dict_suffix_len:
+                    base=dict_suffix_len[next_num]
+                next_num=next_num-1 # dict_suffix_len 中增加不必要的数字
+                base+=1
+                while next_num>=num:
+                    dict_suffix_len[next_num]=base
+                    base+=1
+                    next_num-=1
+            ans=max(dict_suffix_len[num],ans)
+        return ans 
+            
+                        
+if __name__ == "__main__":
+    instance=Solution()
+    instance.longestConsecutive(nums = [100,4,200,1,3,2])
+    instance.longestConsecutive(nums= [0,3,7,2,5,8,4,6,0,1])
