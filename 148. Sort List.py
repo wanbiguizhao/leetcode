d56516a923cb257e3378519bcc3c357e997eb13f
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-04 17:34:58 +0800

    ac 148 把归并排序代码放到内部函数中执行效率高

--- 148. Sort List.py
+++ 148. Sort List.py
@@ -5,16 +5,15 @@ class ListNode:
         self.val = val
         self.next:ListNode = next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def sortList_1(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # 归并排序：两个指针进行排序。
         # 突破点是找到1/2的节点的指针。
         if not head:
             return None 
-        helper_head=ListNode()
-        helper_head.next=head
+        
         pre_p_one_step=None
-        p_one_step=helper_head
-        p_two_step=head
+        p_one_step=head
+        p_two_step=head.next
         while p_one_step and p_two_step:
             pre_p_one_step=p_one_step
             p_one_step=p_one_step.next
@@ -22,17 +21,81 @@ class Solution:
             if not p_two_step:
                 break
             p_two_step.next=p_two_step.next
-        if p_one_step==p_two_step:
+        if p_one_step==head:
             # 当前只剩下一个节点了
-            return helper_head.next
+            return head
 
         pre_p_one_step.next=None# 拆分成两个独立的链表
-        helper
+        helper_head=ListNode()
+        helper_head.next=None 
+        tail_node=helper_head
         one_node=self.sortList(head)# 
         two_node=self.sortList(p_one_step)#
-        while one_node
+        while one_node and two_node:
+            tmp_node=None
+            if one_node.val< two_node.val:
+                tmp_node=one_node
+                one_node=one_node.next
+                tail_node.next=tmp_node
+                tail_node=tmp_node
+            else:
+                tmp_node=two_node
+                two_node=two_node.next 
+                tail_node.next=tmp_node
+                tail_node=tmp_node
+            tmp_node.next=None
 
-        #half_node=head
+        if one_node:
+            tmp_node.next=one_node
+        if two_node:
+            tmp_node.next=two_node
+        return helper_head.next
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 归并排序：两个指针进行排序。
+        # 不能使用递归的方法，只能使用逐步增长步长的方法。
+        if not head:
+            return None 
+        
+        pre_p_one_step=None
+        p_one_step=head
+        p_two_step=head.next
+        while p_one_step and p_two_step:
+            pre_p_one_step=p_one_step
+            p_one_step=p_one_step.next
+            p_two_step=p_two_step.next
+            if not p_two_step:
+                break
+            p_two_step.next=p_two_step.next
+        if p_one_step==head:
+            # 当前只剩下一个节点了
+            return head
+
+        pre_p_one_step.next=None# 拆分成两个独立的链表
+        helper_head=ListNode()
+        helper_head.next=None 
+        tail_node=helper_head
+        one_node=self.sortList(head)# 
+        two_node=self.sortList(p_one_step)#
+        while one_node and two_node:
+            tmp_node=None
+            if one_node.val< two_node.val:
+                tmp_node=one_node
+                one_node=one_node.next
+                tail_node.next=tmp_node
+                tail_node=tmp_node
+            else:
+                tmp_node=two_node
+                two_node=two_node.next 
+                tail_node.next=tmp_node
+                tail_node=tmp_node
+            tmp_node.next=None
+
+        if one_node:
+            tmp_node.next=one_node
+        if two_node:
+            tmp_node.next=two_node
+        return helper_head.next
 
 
         
