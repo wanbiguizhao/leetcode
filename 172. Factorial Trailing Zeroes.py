ae3b91d3b0ed40e771f747f808a9fa9fd3b3e3b4
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-06 11:56:47 +0800

    ac 172 尝试使用reduce 进行计算，速度的确快很多

--- 172. Factorial Trailing Zeroes.py
+++ 172. Factorial Trailing Zeroes.py
@@ -1,3 +1,6 @@
+from cv2 import divide
+
+
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         count=0
@@ -20,8 +23,25 @@ class Solution:
             count=n//5
             n=n//5
         return count 
+    def trailingZeroes(self, n: int) -> int:
+        # 考虑包含5的流程。
+        # 25 这样的情况， 25=5^2 125=5^3 
+        #11!=(2^8*5^2*) 看成5的指数，指数的值可以作为零的个数
+        #   25=5^2 125=5^3  所以要一直做n=n//5的操作。
+        # 尝试使用reduce
+        from functools import reduce
+        divide_list=[0,5,5**2,5**3,5**4,5**5,5**6]
+        def zeroes_helper(res,divid_num):
+            res+=n//divid_num
+            return res 
+        ans=reduce(zeroes_helper,divide_list)
+        print(ans)
+        return ans 
 if __name__ == "__main__":
     instance=Solution()
+    instance.trailingZeroes(4)
+    instance.trailingZeroes(5)
     instance.trailingZeroes(10)
     instance.trailingZeroes(50)
     instance.trailingZeroes(100)
+    instance.trailingZeroes(100000)
