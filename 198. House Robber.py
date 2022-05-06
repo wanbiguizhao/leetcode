f1e4409bde6cc8f3b287bd9b8f336863fd87da4a
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-07 00:42:53 +0800

    ac 198 dp

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -1,3 +1,14 @@
+from typing import List 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
+        const_len=len(nums)
+        if const_len<=2:
+            return max(nums)
+        dp=[0]*const_len
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        index=2
+        while index<const_len:
+            dp[index]=max(dp[index-1],dp[index-2]+nums[index] )
+            index+=1
+        return dp[const_len-1]
