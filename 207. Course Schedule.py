6f77d9015aed90cfe6b72627c3c9a9aa921e68b1
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-07 23:47:56 +0800

    ac 215 ?

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -8,16 +8,29 @@ class Solution:
         # 最后剩余节点，是否为零0 为零true。
         # 0<-1 完成0 必须完成1  
         indegree_dict={x:0 for x in range(numCourses)}
-        outdegree_info_dict={x:[] for x in (numCourses) }
+        outdegree_info_dict={x:[] for x in range(numCourses) }
         # 记录每个节点的入度的值
         # 记录每个节点出度关联的节点。
         for req in prerequisites:
-            indegree_dict[req[1]]+=1
+            indegree_dict[req[0]]+=1
             outdegree_info_dict[ req[1] ].append( req[0] )# 可以告知req[0] 依赖的req[1] 已经完成了
         zero_list=[]
+        ans=numCourses
         for node,indegree in indegree_dict.items():
             if indegree==0:
                 zero_list.append(node )
+               
+        while zero_list:
+            x_node=zero_list.pop(0)
+            ans-=1
+            for z_node in outdegree_info_dict[x_node]:
+                indegree_dict[z_node]-=1
+                if indegree_dict[z_node]==0:
+                    zero_list.append(z_node)
+        return ans==0
 
-        
+
+if __name__=="__main__":
+    instance=Solution()
+    instance.canFinish(2,[1,0])
 
---
+++ 215. Kth Largest Element in an Array.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # 是否存在重复数字的情况？
+        # 堆排序，使用堆排序
+        # 堆排序维持一个长度为k的堆，O(Nlogk)的时间复杂度，K的空间复杂度。
+        pass
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # 使用快排
+        pass
