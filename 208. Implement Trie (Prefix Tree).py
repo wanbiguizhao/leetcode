191d3f8f76c2a50c4776a059efd5ae7b1e7ec628
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-07 16:41:19 +0800

    ac 208

---
+++ 208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,37 @@
+class Trie:
+    # 考察面向对象和数据结构
+
+    def __init__(self):
+        self.core_dict={"$":False}
+        # 通过"$" 区分前缀树的问题。
+        
+
+    def insert(self, word: str) -> None:
+        x_dict=self.core_dict
+        for w in  word:
+            if w in x_dict:
+                x_dict=x_dict[w]
+            else:
+                x_dict[w]={"$":False}
+                x_dict= x_dict[w]
+        x_dict["$"]=True 
+        
+
+    def search(self, word: str) -> bool:
+        x_dict=self.core_dict
+        for w in word:
+            if w in x_dict:
+                x_dict=x_dict[w]
+            else:
+                return False 
+        return x_dict["$"] 
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        x_dict=self.core_dict
+        for w in prefix:
+            if w in x_dict:
+                x_dict=x_dict[w]
+            else:
+                return False 
+        return True 
