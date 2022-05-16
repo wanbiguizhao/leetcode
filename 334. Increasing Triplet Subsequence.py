cec67a7c95eb58fe8fe949015b034d411d8d154a
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-17 00:00:00 +0800

    ac 334

--- 334. Increasing Triplet Subsequence.py
+++ 334. Increasing Triplet Subsequence.py
@@ -1,5 +1,25 @@
-from typing import List,
+from typing import List
+
+from sklearn import tree,
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        # 使用一个特殊的数据结构。单调栈？
-        
+        dp=[] # 使用三个空间复杂度的代码
+        for num in nums:
+            if not dp :
+                dp.append(num)
+            elif len(dp)==1:
+                if num< dp [0]:
+                    dp[0]=num 
+                elif num>dp[0]:
+                    dp.append(num)
+            elif len(dp)==2:
+                if num<dp[0]:
+                    dp[0]=num 
+                elif num<dp[1] and num>dp[0]:
+                    dp[1]=num 
+                elif num>dp[1]:
+                    dp.append(num)
+                    return True 
+        return False 
+            
+                
