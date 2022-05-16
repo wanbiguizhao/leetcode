983a1f07e7972fe024fecb97fc1d73695cf2e339
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-16 18:45:46 +0800

    ac 300 O(n**2) dp

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,9 +1,11 @@
-from typing import List 
+import re
+from typing import List
+
+from urllib3 import Retry 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # pass 
         cache={0:0}
-        
         coins.sort()# 排序，使用自底向上的方法去弄
         # 对于 [1,4,5] 8 的输入测试案例，并不能使用贪心法
         # 还是自底向上构架吧。
@@ -11,7 +13,7 @@ class Solution:
             cache[coin]=1 
         ans=0
         x=coins[0]
-        while x<=amount:
+        while x<amount:
             x+=1
             if x in cache:
                 continue
@@ -22,7 +24,7 @@ class Solution:
                             if x in cache:
                                 cache[x]=min(1+cache[x-coin],cache[x])
                             else:
-                                cache[x]=cache[x-coin]
+                                cache[x]=cache[x-coin]+1
                     else:
                         break 
             
@@ -30,3 +32,30 @@ class Solution:
             return cache[amount]
         return -1
 
+def coinChange(self, coins: List[int], amount: int) -> int:
+        # 尝试BFS，存在大量的重复答案。
+        coins.sort()# 排序，使用自底向上的方法去弄
+        # 对于 [1,4,5] 8 的输入测试案例，并不能使用贪心法
+        # 还是自底向上构架吧。
+        if amount==0:
+            return 0
+        if amount<coins[0]:
+            return -1 
+        ans=0 
+        bfs_queue=[0]
+        while bfs_queue:
+            tmp_set=set()
+            ans+=1
+            for x in bfs_queue:
+                if x>amount: # 剪枝法
+                    continue
+                for coin in coins:
+                    if (x+coin)==amount:
+                        return ans  
+                    elif x+coin<amount:
+                        tmp_set.add(x+coin) # 这个可能已经有过计算过的答案了
+                    else:
+                        break
+            bfs_queue=list(tmp_set)
+            bfs_queue.sort()
+        return -1
