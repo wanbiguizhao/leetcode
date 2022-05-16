93995e6aad2e523f279a94d658538762710c5265
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-17 00:15:56 +0800

    334 标准代码时间消耗也挺长的

--- 334. Increasing Triplet Subsequence.py
+++ 334. Increasing Triplet Subsequence.py
@@ -4,6 +4,7 @@ from sklearn import tree,
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         dp=[num] # 使用三个空间复杂度的代码
+
         for num in nums:
             if not dp :
                 dp.append(num)
@@ -21,5 +22,25 @@ class Solution:
                     dp.append(num)
                     return True 
         return False 
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        dp=[nums[0]] # 使用三个空间复杂度的代码
+        # 深刻理解，扑克牌，每次把最小的放到合适位置的算法
+        count=1
+        for index in range(1,len(nums)):
+            if dp[-1]<nums[index]:
+                if count==2:
+                    return True 
+                else:
+                    dp.append(nums[index])
+                    count+=1
+            elif dp[-1]>nums[index]:
+                if count==2 : 
+                    if nums[index]>dp[0] :
+                        dp[-1]=nums[index]
+                    elif nums[index]<dp[0]:
+                        dp[0]=nums[index]
+                else:
+                    dp[-1]=nums[index]
+        return False
             
-                
+                    
