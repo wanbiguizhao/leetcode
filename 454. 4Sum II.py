2247d4fe595534fb384bc942332c1253b5cecda4
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-20 11:56:47 +0800

    tle

--- 454. 4Sum II.py
+++ 454. 4Sum II.py
@@ -1,4 +1,18 @@
-from typing import List 
+from typing import List
+from collections import defaultdict 
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        def product(numsA,numsB):
+            ans_dict=defaultdict(0)
+            for a in numsA:
+                for b in numsB:
+                    ans_dict[a+b]+=1
+            return ans_dict 
+        add_12_dict=product(nums1,nums2)
+        add_34_dict=product(nums3,nums4)
+        ans=0
+        for val,cnt in add_12_dict.items():
+            if -val in add_34_dict:
+                ans+=cnt*add_34_dict[-val]
+        return ans 
         
