4eecf87d4615e67d004c0759332a175801b2aaa0
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-22 02:57:43 +0800

    ac 42

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -12,21 +12,21 @@ class Solution:
             for i in range(0,const_list_amount-interval,interval*2):
                 # 为什么上限是： const_list_amount-interval，因为要确保，i,i+interval是可以合法的。
                 lists[i]=self.merge2Lists(lists[i],lists[i+interval])
-                interval*=2 # 这也是一个非常重要的点
-            return lists[0] if const_list_amount>0 else None 
-        def merge2Lists(self,l1:ListNode,l2:ListNode):
-            head=point=ListNode()
-            while l1 and l2:
-                if l1.val <=l2.val:
-                    point.next=l1
-                    l1=l1.next
-                else:
-                    point.next=l2 
-                    l2=l2.next
-                point=point.next
-                point.next=None
-            if not l1:
-                point.next=l2 
+            interval*=2 # 这也是一个非常重要的点
+        return lists[0] if const_list_amount>0 else None 
+    def merge2Lists(self,l1:ListNode,l2:ListNode):
+        head=point=ListNode()
+        while l1 and l2:
+            if l1.val <=l2.val:
+                point.next=l1
+                l1=l1.next
             else:
-                point.next-l1 
-            return head.next
+                point.next=l2 
+                l2=l2.next
+            point=point.next
+            point.next=None
+        if not l1:
+            point.next=l2 
+        else:
+            point.next=l1 
+        return head.next
--- 42. Trapping Rain Water.py
+++ 42. Trapping Rain Water.py
@@ -1,4 +1,24 @@
 from typing import List 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pass 
+        # 1.  对于元素i，高度为height[i]
+        # 2. 左边的情况找到最高的元素left_max_height
+        # 3. 右边可以找到最高的边right_max_height
+        # 4. 元素i可以保留的水是:max(0,min(left_max_height,right_max_height)-height[i])
+        const_height_len=len(height)
+        left_max_list=[0]*const_height_len
+        left_max_list[0]=height[0]
+        right_max_list=[0]*const_height_len
+        right_max_list[const_height_len-1]=height[const_height_len-1]
+        for index in range(1,const_height_len-1):
+            left_max_list[index]=max(height[index-1],left_max_list[index-1])
+        for index in range(const_height_len-2,0,-1):
+            right_max_list[index]=max(height[index+1],right_max_list[index+1])
+        ans=0
+        for index in range(1,const_height_len-1):
+            ans+=max( min(left_max_list[index],right_max_list[index]) -height[index] ,0)
+        return ans 
+if __name__ == "__main__":
+    instance=Solution()
+    instance.trap(height = [0,1,0,2,1,0,1,3,2,1,2,1])
+    instance.trap(height =[4,2,0,3,2,5])
