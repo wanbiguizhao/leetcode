5f4cf5670270e7fefa2feacf2f008c5c50759410
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-05-24 03:18:20 +0800

    ac 44 减少了从重复解

--- 44. Wildcard Matching.py
+++ 44. Wildcard Matching.py
@@ -73,12 +73,71 @@ class Solution:
                 
         for i in range(1, len(s)+1):
             for j in range(1, len(p)+1):
-                if p[j-1] in {s[i-1], '?'}:
+                if p[j-1] ==s[i-1] or p[j-1]=='?':
                     dp[i][j] = dp[i-1][j-1]
                 elif p[j-1] == '*':
                     dp[i][j] = dp[i-1][j] or dp[i][j-1]
         return dp[-1][-1]
 
+    def isMatch(self, s: str, p: str) -> bool:
+        # 递归  p[0]==*
+        # 递归  p[0]==?
+        #       p[0]字符串。 
+        # 对p进行初始化处理，替换到多余的*
+        # 使用缓存进行处理
+        def doMatch(si:int,pi:int):
+            if (si,pi) in cache:
+                return cache[(si,pi)]
+            if  s and  p:
+                i=0
+                while  s[i]==p[i]:
+                    i=i+1
+                    if i==len(p) or i ==len(s):
+                        return doMatch(s[i:],p[i:])
+                if p[i]=="*":
+                    if doMatch(s[i:],p[i+1:]):
+                        return True 
+                    elif doMatch(s[i+1:],p[i+1:]):
+                        return True 
+                    elif doMatch(s[i+1:],p[i:]):
+                        return True 
+                    return False 
+                if p[i]=="?":
+                    return doMatch(s[i+1:],p[i+1:])
+                return False 
+            elif  s and not p:
+                return False 
+            elif p and not s:
+                if p[0]=="*":
+                    return doMatch(s,p[1:])
+                return False
+            else:
+                return True  
+
+        new_p="^"
+        for x in p:
+            if new_p[-1]=="*" and x=="*":
+                continue 
+            else:
+                new_p+=x
+        # 需要进行一些减枝条算法
+        p=new_p[1:]
+        # i=0
+        
+        # while i<len(p) and i <len(s) and   p[i]!="*" and p[i]!="?":
+        #     if p[i]!=s[i]:
+        #         return False
+        #     i=i+1
+
+        # i=-1
+        # while  i+len(p)>=0 and i+len(s)>=0 and    p[i]!="*" and p[i]!="?":
+        #     if p[i]!=s[i]:
+        #         return False
+        #     i=i-1
+
+        # #p=new_p[1:]
+        cache={}
+        return doMatch(0,0)
 if __name__ == "__main__":
     instance=Solution()
     # print(instance.isMatch(s = "aa", p = "a")==False)
