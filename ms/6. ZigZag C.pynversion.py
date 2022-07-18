6fcb02faa43753118ba59ecee7a781da7faa9c89
wanbiguizhao <shiweishiweihubugui@live.cn>
2022-07-18 12:38:14 +0800

    新的方法

--- ms/6. ZigZag C.pynversion.py
+++ ms/6. ZigZag C.pynversion.py
@@ -3,13 +3,12 @@ from cmath import inf
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        # 数学公式计算，然后进行排序 
+        # 数学公式计算，可以得出zig存在两个方向的变化。
         if numRows==1:
             return s 
         down_direction=[1,0]
         right_up_direction=[-1,1]
         directions=[]
-
         for i in range(numRows-1):
             directions.append(down_direction)
         for i in range(numRows-1):
@@ -24,6 +23,38 @@ class Solution:
             directionIndex%=2*(numRows-1)
         ansCoordinateList.sort()
         return "".join([ info[-1] for info in ansCoordinateList])# 把字符串拼接起来
+
+    def convert(self, s: str, numRows: int) -> str:
+        # 数学公式计算，可以得出zig存在两个方向的变化。
+        if numRows==1:
+            return s 
+        down_direction=[1,0]
+        right_up_direction=[-1,1]
+        directions=[]
+        for i in range(numRows-1):
+            directions.append(down_direction)
+        for i in range(numRows-1):
+            directions.append(right_up_direction)
+        ans_cache={}
+        ans_cache[0]=[s[0]]
+        currentCoordinateX=0
+        currentCoordinateY=0
+        ansCoordinateList=[[0,0,s[0]]]
+        directionIndex=0
+        for x in s[1:]:
+            direction=directions[directionIndex]
+            currentCoordinateX=currentCoordinateX+direction[0]
+            currentCoordinateY=currentCoordinateY+direction[1]
+            if currentCoordinateX not in ans_cache:
+                ans_cache[currentCoordinateX]=[]
+            ans_cache[currentCoordinateX].append(x)
+            directionIndex+=1
+            directionIndex%=2*(numRows-1)
+        ans=""
+        for row in range(numRows):
+            ans+="".join(ans_cache.get(row,""))
+        return ans
+        
 def testCase0(instance:Solution=Solution()):
     print(instance.convert("Abcd",2))
     print(instance.convert("Abcd",1))
@@ -33,7 +64,8 @@ def testCase0(instance:Solution=Solution()):
 
 
 def wrongCase0(instance:Solution=Solution()):
-    pass 
+    print(instance.convert(s = "A", numRows = 2)=="A")
+
 
 
 
