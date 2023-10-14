c2fab3a6c17a02d6e83b8d023d72941a661b98ca
wanbiguizhao <shiweishiweihubugui@live.cn>
2023-10-14 22:49:19 +0800

    Auto saved by Logseq

--- questionbank/179.最大数.py
+++ questionbank/179.最大数.py
@@ -1,4 +1,7 @@
+from typing import List
 class Solution:
   	def largestNumber(self, nums: List[int]):
-      	sorted(nums,k)
+		def compare(a:int,b:int):
+			return str(a)+str(b)>str(b)+str(a)
+      	sorted(nums,key=compare,)
       
